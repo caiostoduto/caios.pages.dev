8589c08fe4fdfb106861089a2a3d68fe6bf2ba3b
Caio Stoduto <88816833+caiostoduto@users.noreply.github.com>
2024-05-01 01:33:44 -0300

    chore: format @/app/api/redirect

--- src/app/api/redirect/route.ts
+++ src/app/api/redirect/route.ts
@@ -1,47 +1,49 @@
 // Next.js Edge API Route Handlers: https://nextjs.org/docs/app/building-your-application/routing/router-handlers#edge-and-nodejs-runtimes
 
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from "next/server";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  // In the edge runtime you can use Bindings that are available in your application
-  // (for more details see:
-  //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
-  //    - https://developers.cloudflare.com/pages/functions/bindings/
-  // )
-  //
-  // KV Example:
-  // const myKv = process.env.MY_KV
-  // await myKv.put('suffix', ' from a KV store!')
-  // const suffix = await myKv.get('suffix')
-  // responseText += suffix
+	// In the edge runtime you can use Bindings that are available in your application
+	// (for more details see:
+	//    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
+	//    - https://developers.cloudflare.com/pages/functions/bindings/
+	// )
+	//
+	// KV Example:
+	// const myKv = process.env.MY_KV
+	// await myKv.put('suffix', ' from a KV store!')
+	// const suffix = await myKv.get('suffix')
+	// responseText += suffix
 
-  const url = new URL(request.url)
-  var query = url.searchParams.get('q')?.toLocaleLowerCase()
+	const url = new URL(request.url);
+	let query = url.searchParams.get("q")?.toLocaleLowerCase();
 
-  if (query === undefined || query === '') {
-    query = '/' // return new Response('Missing query', { status: 400 })
-  }
+	if (query === undefined || query === "") {
+		query = "/"; // return new Response('Missing query', { status: 400 })
+	}
 
-  return await returnKVResponse(query)
+	return await returnKVResponse(query);
 }
 
 async function returnKVResponse(query: string): Promise<Response> {
-  const kv = process.env.REDIRECT;
-  const value = await kv.get(query)
+	const kv = process.env.REDIRECT;
+	const value = await kv.get(query);
 
-  if (value !== null) {
-    return new Response(
-      JSON.stringify({
-        url: value,
-        default: query === '/'
-      }), { status: 200 })
-  } else {
-    if (query !== '/') {
-      return await returnKVResponse('/')
-    } else {
-      return new Response('Not found', { status: 404 })
-    }
-  }
+	if (value !== null) {
+		return new Response(
+			JSON.stringify({
+				url: value,
+				default: query === "/",
+			}),
+			{ status: 200 },
+		);
+	}
+
+	if (query !== "/") {
+		return await returnKVResponse("/");
+	}
+
+	return new Response("Not found", { status: 404 });
 }
